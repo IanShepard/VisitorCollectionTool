56f925fdf519d967e78a6e49819b60f23ec51523
Kylie Harmon <kharmon3@bsu.edu>
2019-11-14 18:28:35 -0500

    findName

--- src/checkin.php
+++ src/checkin.php
@@ -21,4 +21,41 @@
 	</body>
 </html>
 
-<?php?>
+<?php
+if(!empty($_POST))
+{
+	if(!empty($_POST["name"]))
+	{
+		$name = $_POST["name"];
+		if(($file = fopen("test.csv", "r+")) !== FALSE)
+		{
+			$info =  readCSV($file);
+		}
+		else
+		{
+			echo "File Not Found!";
+		}
+
+		$names = $findName($name, $info);
+		echo '<table border = 1>';
+		for ($i = 0; $i < sizeof($names); $i++)
+		{
+			echo '<tr>';
+			for ($j = 0; $j < 3; $j++)
+			{
+				echo '<td>'.$names[$j].'</td>';
+			}
+			echo '</tr';
+		}
+		echo '</table>';
+
+		fclose($file);
+	}
+		else
+		{
+			echo '<script language="javascript">';
+			echo 'alert("Some fields are empty, try again!")';
+			echo '</script>';
+		}
+	}
+		?>
