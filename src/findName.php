e058331abc0ceb24b0f1233114a51ff1b0001322
Kylie Harmon <kharmon3@bsu.edu>
2019-11-14 18:29:41 -0500

    findName update

---
+++ src/findName.php
@@ -0,0 +1,25 @@
+<?php
+function findName($fullName, $info)
+    {
+        $name = exlpode($fullName, " ");
+        $fname = $name [0];
+        $lname =  $name [1];
+        $matchL = array();
+        for ($i = 0; i < sizeof($info); $i++)
+        {
+            if ($info[$i][1] == $lname)
+            {
+                array_push($matchL, $info[i]);
+            }
+        }
+        $matchF = array();
+        for ($i = 0; i < sizeof($matchL); $i++)
+        {
+            if ($info[$i][0] == $fname)
+            {
+                array_push($matchF, $matchL[i]);
+            }
+        }
+        return $matchF;
+	}
+?>
