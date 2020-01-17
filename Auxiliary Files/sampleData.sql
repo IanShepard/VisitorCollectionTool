d86af54edac7090a90c00358c30e8fd18a53cf9b
mdnessel <35309158+mdnessel@users.noreply.github.com>
2020-01-17 15:52:58 -0500

    Add sampleData.sql for functionality testing purposes

---
+++ Auxiliary Files/sampleData.sql
@@ -0,0 +1,7 @@
+INSERT INTO event(`Eventid`, `Name`, `Date`) VALUES(200, "Sample Event", "2020-02-01");
+
+INSERT INTO attendee(Fname, Lname, Email, Eventid, Attended) VALUES("Jane", "Doe", "jdoe@bsu.edu", 200, FALSE);
+INSERT INTO attendee(Fname, Lname, Email, Eventid, Attended) VALUES("John", "Smith", "jsmith@bsu.edu", 200, FALSE);
+INSERT INTO attendee(Fname, Lname, Email, Eventid, Attended) VALUES("Mike", "Wazowski", "mwazowski@gmail.com", 200, FALSE);
+INSERT INTO attendee(Fname, Lname, Email, Eventid, Attended) VALUES("James", "Sullivan", "jsullivan@yahoo.com", 200, FALSE);
+INSERT INTO attendee(Fname, Lname, Email, Eventid, Attended) VALUES("Bob", "Jones", "bjones@outlook.com", 200, FALSE);
