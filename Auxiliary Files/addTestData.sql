ef97946fcefbca493498af6dfb53668dbb2e0de3
mdnessel <35309158+mdnessel@users.noreply.github.com>
2020-01-17 15:35:25 -0500

    Add SQL scripts for environment replication

---
+++ Auxiliary Files/addTestData.sql
@@ -0,0 +1,5 @@
+INSERT INTO event(Eventid,Name,Date) VALUES(999, "Test Event", "2020-04-01");
+INSERT INTO attendee(Fname,Lname,Email,Eventid,Attended) VALUES("Bob", "Jones", "bjones@gmail.com",999,0);
+INSERT INTO attendee(Fname,Lname,Email,Eventid,Attended) VALUES("Mary", "Jane", "mjane@gmail.com",999,1);
+INSERT INTO attendee(Fname,Lname,Email,Eventid,Attended) VALUES("John", "Smith", "jsmith@gmail.com",999,0);
+INSERT INTO attendee(Fname,Lname,Email,Eventid,Attended) VALUES("Bob", "Jones", "bjones2@gmail.com",999,0);
---
+++ Auxiliary Files/createEventTable.sql
@@ -0,0 +1,1 @@
+CREATE TABLE event(Eventid int(6) NOT NULL AUTO_INCREMENT, `Name` VARCHAR(50) NOT NULL, `Date` date NOT NULL, PRIMARY KEY(Eventid)) ENGINE = InnoDB; 
