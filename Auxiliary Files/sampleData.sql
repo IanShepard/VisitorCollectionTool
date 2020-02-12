fb350df9b22e0a2662dbca6fed2dc07f44bd355e
mdnessel <35309158+mdnessel@users.noreply.github.com>
2020-02-12 11:40:44 -0500

    Update sampleData.sql

--- Auxiliary Files/sampleData.sql
+++ Auxiliary Files/sampleData.sql
@@ -1,7 +1,13 @@
-INSERT INTO event(`Eventid`, `Name`, `Date`) VALUES(200, "Sample Event", "2020-02-01");
+INSERT INTO event(`Eventid`, `Name`, `Date`) VALUES(200, "Sample Event", "2021-02-01");
 
-INSERT INTO attendee(Fname, Lname, Email, Eventid, Attended) VALUES("Jane", "Doe", "jdoe@bsu.edu", 200, FALSE);
-INSERT INTO attendee(Fname, Lname, Email, Eventid, Attended) VALUES("John", "Smith", "jsmith@bsu.edu", 200, FALSE);
-INSERT INTO attendee(Fname, Lname, Email, Eventid, Attended) VALUES("Mike", "Wazowski", "mwazowski@gmail.com", 200, FALSE);
-INSERT INTO attendee(Fname, Lname, Email, Eventid, Attended) VALUES("James", "Sullivan", "jsullivan@yahoo.com", 200, FALSE);
-INSERT INTO attendee(Fname, Lname, Email, Eventid, Attended) VALUES("Bob", "Jones", "bjones@outlook.com", 200, FALSE);
+INSERT INTO attendee(`Fname`, `Lname`, `Phone`, `Email`) VALUES("Jane", "Doe", "555-5555", "jdoe@bsu.edu");
+INSERT INTO attendee(`Fname`, `Lname`, `Phone`, `Email`) VALUES("John", "Smith", "555-5556", "jsmith@bsu.edu");
+INSERT INTO attendee(`Fname`, `Lname`, `Phone`, `Email`) VALUES("Mike", "Wazowski", "555-5557", "mwazowski@gmail.com");
+INSERT INTO attendee(`Fname`, `Lname`, `Phone`, `Email`) VALUES("James", "Sullivan", "555-5558", "jsullivan@yahoo.com");
+INSERT INTO attendee(`Fname`, `Lname`, `Phone`, `Email`) VALUES("Bob", "Jones", "555-5559", "bjones@outlook.com");
+
+INSERT INTO attendance(`Attendeeid`, `Eventid`, `Registered`) VALUES(1, 200, TRUE);
+INSERT INTO attendance(`Attendeeid`, `Eventid`, `Registered`) VALUES(2, 200, TRUE);
+INSERT INTO attendance(`Attendeeid`, `Eventid`, `Registered`) VALUES(3, 200, TRUE);
+INSERT INTO attendance(`Attendeeid`, `Eventid`, `Registered`) VALUES(4, 200, TRUE);
+INSERT INTO attendance(`Attendeeid`, `Eventid`, `Registered`) VALUES(5, 200, TRUE);
